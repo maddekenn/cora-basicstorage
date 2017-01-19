dab0cbd4b481472bd72c4afa8a03a7684e9f61bd
olov <olov.mckie@ub.uu.se>
2017-01-19 16:13:32 +0100

    adding support for AppTokenStorage

---
+++ src/main/java/se/uu/ub/cora/storage/AppTokenStorageImp.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2017 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.uu.ub.cora.storage;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import se.uu.ub.cora.apptokenverifier.AppTokenStorage;
+import se.uu.ub.cora.bookkeeper.data.DataGroup;
+import se.uu.ub.cora.spider.record.storage.RecordNotFoundException;
+
+public class AppTokenStorageImp implements AppTokenStorage {
+
+	protected static final String RECORD_TYPE = "recordType";
+	private static final String PARENT_ID = "parentId";
+	RecordStorageInMemoryReadFromDisk recordStorage;
+	private List<String> userRecordTypeNames = new ArrayList<>();
+
+	public AppTokenStorageImp(Map<String, String> initInfo) {
+		if (!initInfo.containsKey("storageOnDiskBasePath")) {
+			throw new RuntimeException("initInfo must contain storageOnDiskBasePath");
+		}
+		recordStorage = RecordStorageInMemoryReadFromDisk
+				.createRecordStorageOnDiskWithBasePath(initInfo.get("storageOnDiskBasePath"));
+		populateUserRecordTypeNameList();
+	}
+
+	@Override
+	public List<String> getApptokensForUser(String userId) {
+		DataGroup user = findUser(userId);
+		return getAppTokensForUser(user);
+	}
+
+	private DataGroup findUser(String userId) {
+		DataGroup user = null;
+		for (String userRecordTypeName : userRecordTypeNames) {
+			try {
+				user = recordStorage.read(userRecordTypeName, userId);
+			} catch (RecordNotFoundException e) {
+				// do nothing
+			}
+		}
+		return user;
+	}
+
+	private List<String> getAppTokensForUser(DataGroup user) {
+		if (userExistsAndIsActive(user)) {
+			return getAppTokensForActiveUser(user);
+		}
+		return new ArrayList<>();
+	}
+
+	private boolean userExistsAndIsActive(DataGroup user) {
+		return user != null && userIsActive(user);
+	}
+
+	private List<String> getAppTokensForActiveUser(DataGroup user) {
+		List<DataGroup> userAppTokenGroups = user.getAllGroupsWithNameInData("userAppTokenGroup");
+		return getAppTokensForAppTokenGroups(userAppTokenGroups);
+	}
+
+	private List<String> getAppTokensForAppTokenGroups(List<DataGroup> userAppTokenGroups) {
+		List<String> apptokens = new ArrayList<>();
+		for (DataGroup userAppTokenGroup : userAppTokenGroups) {
+			String appTokenId = extractAppTokenId(userAppTokenGroup);
+			apptokens.add(getTokenFromStorage(appTokenId));
+		}
+		return apptokens;
+	}
+
+	private String extractAppTokenId(DataGroup userAppTokenGroup) {
+		return userAppTokenGroup.getFirstGroupWithNameInData("appTokenLink")
+				.getFirstAtomicValueWithNameInData("linkedRecordId");
+	}
+
+	private String getTokenFromStorage(String appTokenId) {
+		return recordStorage.read("appToken", appTokenId)
+				.getFirstAtomicValueWithNameInData("token");
+	}
+
+	private boolean userIsActive(DataGroup user) {
+		return "active".equals(user.getFirstAtomicValueWithNameInData("activeStatus"));
+	}
+
+	private void populateUserRecordTypeNameList() {
+		Collection<DataGroup> recordTypes = recordStorage.readList(RECORD_TYPE);
+
+		for (DataGroup recordTypePossibleChild : recordTypes) {
+			addChildOfUserToUserRecordTypeNameList(recordTypePossibleChild);
+		}
+	}
+
+	private void addChildOfUserToUserRecordTypeNameList(DataGroup recordTypePossibleChild) {
+		if (isChildOfUserRecordType(recordTypePossibleChild)) {
+			addChildToReadRecordList(recordTypePossibleChild);
+		}
+	}
+
+	protected boolean isChildOfUserRecordType(DataGroup recordTypePossibleChild) {
+		return recordHasParent(recordTypePossibleChild)
+				&& userRecordTypeIsParentToRecord(recordTypePossibleChild);
+	}
+
+	private void addChildToReadRecordList(DataGroup recordTypePossibleChild) {
+		String childRecordType = recordTypePossibleChild.getFirstGroupWithNameInData("recordInfo")
+				.getFirstAtomicValueWithNameInData("id");
+		userRecordTypeNames.add(childRecordType);
+	}
+
+	private boolean recordHasParent(DataGroup handledRecordTypeDataGroup) {
+		return handledRecordTypeDataGroup.containsChildWithNameInData(PARENT_ID);
+	}
+
+	private boolean userRecordTypeIsParentToRecord(DataGroup recordTypePossibleChild) {
+		return "user".equals(recordTypePossibleChild.getFirstAtomicValueWithNameInData(PARENT_ID));
+	}
+
+}
---
+++ src/test/java/se/uu/ub/cora/storage/AppTokenStorageTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2017 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.uu.ub.cora.storage;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import se.uu.ub.cora.storage.testdata.TestDataAppTokenStorage;
+
+public class AppTokenStorageTest {
+	private String basePath = "/tmp/recordStorageOnDiskTemp/";
+	private Map<String, String> initInfo;
+	private AppTokenStorageImp appTokenStorage;
+
+	@BeforeMethod
+	public void makeSureBasePathExistsAndIsEmpty() throws IOException {
+		File dir = new File(basePath);
+		dir.mkdir();
+		deleteFiles();
+		TestDataAppTokenStorage.createRecordStorageInMemoryWithTestData(basePath);
+
+		initInfo = new HashMap<>();
+		initInfo.put("storageOnDiskBasePath", basePath);
+		appTokenStorage = new AppTokenStorageImp(initInfo);
+	}
+
+	private void deleteFiles() throws IOException {
+		Stream<Path> list;
+		list = Files.list(Paths.get(basePath));
+		list.forEach(p -> deleteFile(p));
+		list.close();
+	}
+
+	private void deleteFile(Path path) {
+		try {
+			Files.delete(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterMethod
+	public void removeTempFiles() throws IOException {
+		if (Files.exists(Paths.get(basePath))) {
+			deleteFiles();
+			File dir = new File(basePath);
+			dir.delete();
+		}
+	}
+
+	@Test
+	public void test() {
+		assertTrue(appTokenStorage.recordStorage instanceof RecordStorageInMemoryReadFromDisk);
+	}
+
+	@Test(expectedExceptions = RuntimeException.class)
+	public void testInitNoStorageOnDiskBasePath() {
+		Map<String, String> initInfo = new HashMap<>();
+		new AppTokenStorageImp(initInfo);
+	}
+
+	@Test
+	public void testGetAppTokenForUser() {
+		List<String> apptokensForUser = appTokenStorage.getApptokensForUser("dummy1");
+		assertNotNull(apptokensForUser);
+		assertEquals(apptokensForUser.get(0), "someSecretString");
+	}
+
+	@Test
+	public void testGetAppTokenForUser2() {
+		List<String> apptokensForUser = appTokenStorage.getApptokensForUser("dummy2");
+		assertNotNull(apptokensForUser);
+		assertEquals(apptokensForUser.get(0), "someOtherSecretString");
+	}
+
+	@Test
+	public void testGetAppTokenForNonExistingUser() {
+		List<String> apptokensForUser = appTokenStorage.getApptokensForUser("nonExistingUser");
+		assertNotNull(apptokensForUser);
+		assertEquals(apptokensForUser.size(), 0);
+	}
+
+	@Test
+	public void testGetAppTokenForInactiveUser() {
+		List<String> apptokensForUser = appTokenStorage.getApptokensForUser("inactiveUser");
+		assertNotNull(apptokensForUser);
+		assertEquals(apptokensForUser.size(), 0);
+	}
+
+	@Test
+	public void testGetAppTokenForUserNoAppToken() {
+		List<String> apptokensForUser = appTokenStorage.getApptokensForUser("noAppTokenUser");
+		assertNotNull(apptokensForUser);
+		assertEquals(apptokensForUser.size(), 0);
+	}
+}
