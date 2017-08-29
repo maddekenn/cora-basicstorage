87d2c1c9a8374cd9d9b9ba4e81e398fbd22a1ab4
madde <madde@0ff659e02cf2>
2017-08-29 19:45:57 +0000

    CORA-382, adding searchStorageImp

---
+++ src/test/java/se/uu/ub/cora/storage/SearchStorageTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2017 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.uu.ub.cora.storage;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import se.uu.ub.cora.bookkeeper.data.DataGroup;
+import se.uu.ub.cora.storage.testdata.TestDataSearchStorage;
+
+public class SearchStorageTest {
+	private String basePath = "/tmp/recordStorageOnDiskTemp/";
+	private Map<String, String> initInfo;
+	private SearchStorageImp searchStorage;
+
+	@BeforeMethod
+	public void makeSureBasePathExistsAndIsEmpty() throws IOException {
+		File dir = new File(basePath);
+		dir.mkdir();
+		deleteFiles();
+		TestDataSearchStorage.createRecordStorageInMemoryWithTestData(basePath);
+
+		initInfo = new HashMap<>();
+		initInfo.put("storageOnDiskBasePath", basePath);
+		searchStorage = new SearchStorageImp(initInfo);
+	}
+
+	private void deleteFiles() throws IOException {
+		Stream<Path> list;
+		list = Files.list(Paths.get(basePath));
+		list.forEach(p -> deleteFile(p));
+		list.close();
+	}
+
+	private void deleteFile(Path path) {
+		try {
+			Files.delete(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterMethod
+	public void removeTempFiles() throws IOException {
+		if (Files.exists(Paths.get(basePath))) {
+			deleteFiles();
+			File dir = new File(basePath);
+			dir.delete();
+		}
+	}
+
+	@Test
+	public void test() {
+		assertTrue(searchStorage.recordStorage instanceof RecordStorageInMemoryReadFromDisk);
+	}
+
+	@Test(expectedExceptions = RuntimeException.class)
+	public void testInitNoStorageOnDiskBasePath() {
+		Map<String, String> initInfo = new HashMap<>();
+		new UserStorageImp(initInfo);
+	}
+
+	@Test
+	public void testGetSearchTerm() {
+		DataGroup userGroup = searchStorage.getSearchTerm("someSearchTerm");
+		assertEquals(userGroup.getFirstGroupWithNameInData("recordInfo")
+				.getFirstAtomicValueWithNameInData("id"), "someSearchTerm");
+	}
+	//
+	// @Test
+	// public void testGetUserDummy1() {
+	// DataGroup userGroup = userStorage.getUserById("dummy1");
+	// assertEquals(userGroup.getFirstGroupWithNameInData("recordInfo")
+	// .getFirstAtomicValueWithNameInData("id"), "dummy1");
+	// }
+	//
+	// @Test
+	// public void testGetUserRepopulatesOnceFromDiskIfNotFound() {
+	// DataGroup userGroup = userStorage.getUserById("unKnownUser");
+	// assertNull(userGroup);
+	// assertEquals(userStorage.getNoOfReadsFromDisk(), 2);
+	// }
+
+}
