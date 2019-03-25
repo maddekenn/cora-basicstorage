15ac8b04576805947ca0f13975f50a052ab846b4
olovm <olov.mckie@ub.uu.se>
2019-03-25 12:01:14 +0000

    updating to 1.0 for module change

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module se.uu.ub.cora.basicstorage {
-	requires se.uu.ub.cora.gatekeeper;
-	requires spider;
-	requires json;
-	requires apptokenstorage;
-	requires searchstorage;
+	requires transitive se.uu.ub.cora.gatekeeper;
+	requires transitive se.uu.ub.cora.bookkeeper;
+	requires se.uu.ub.cora.json;
+	requires se.uu.ub.cora.apptokenstorage;
+	requires se.uu.ub.cora.searchstorage;
+	requires transitive se.uu.ub.cora.spider;
 
 	exports se.uu.ub.cora.storage;
 
